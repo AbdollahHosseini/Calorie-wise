816cc548da8f50834e1d912a2c1b4ea1c43c9189
AbdollahHosseini <unihosseini.a@gmail.com>
2025-04-05 17:12:19 +0100

    file structure with spring boot

---
+++ src/main/java/com/example/calorieInc/Calorie_wise/CalorieWiseApplication.java
@@ -0,0 +1,13 @@
+package com.example.calorieInc.Calorie_wise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalorieWiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalorieWiseApplication.class, args);
+	}
+
+}

350154d14b96a49f5e6f81853043e45f49bb2991
AbdollahHosseini <unihosseini.a@gmail.com>
2025-04-05 17:34:33 +0100

    initial page working

---
+++ src/test/java/com/example/calorieInc/Calorie_wise/CalorieWiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.calorieInc.Calorie_wise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalorieWiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
